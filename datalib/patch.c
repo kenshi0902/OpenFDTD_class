59af3cda94adadfff0aaa9f2d63cd20b68d1f398
kenshi0902 <kenshi0902@gmail.com>
2023-11-29 17:32:34 +0900

    Tab indent

--- datalib/patch.c
+++ datalib/patch.c
@@ -1,6 +1,6 @@
 /*
-	gcc -O patch.c ofd_datalib.c -o patch
-	./patch
+    gcc -O patch.c ofd_datalib.c -o patch
+    ./patch
 */
 
 
@@ -10,16 +10,16 @@
 
 int main(void){
     const double freq = 2.5e9;
-	const double lambda = 3e8 / freq;
-	const double dt = 1.6e-3;           // Dielectric thickness
-	const double A = 100e-3;            // Dielectric X-size
-	const double B = 140e-3;			// Dielecrtic Y-size
-	const double margin1 = 0.5*lambda;
-	const double margin2 = 0.25*lambda;
-	const double er = 4.3;
-	const double tand = 0.001;
-	const double eps0 = 8.854e-12;
-	const double pi = 4 * atan(1);
+    const double lambda = 3e8 / freq;
+    const double dt = 1.6e-3;           // Dielectric thickness
+    const double A = 100e-3;            // Dielectric X-size
+    const double B = 140e-3;			// Dielecrtic Y-size
+    const double margin1 = 0.5*lambda;
+    const double margin2 = 0.25*lambda;
+    const double er = 4.3;
+    const double tand = 0.001;
+    const double eps0 = 8.854e-12;
+    const double pi = 4 * atan(1);
     const double sigma = er * tand * (2 * pi * freq) * eps0;
 
     // ===Parameter===
@@ -30,52 +30,52 @@ int main(void){
     const double l_de = 10.8e-3;        // Line depth
     // ===============
 
-	// initialize
+    // initialize
 
-	ofd_init();
+    ofd_init();
 
-	// title
+    // title
 
-	ofd_title("Patch Antenna");
+    ofd_title("Patch Antenna");
 
-	// mesh
+    // mesh
     // x
     const double x0 = A/2;
     const double x1 = W/2;
     const double x2 = w_li/2 + w_sl;
     const double x3 = w_li/2;
-	const int xmesh_dielectric = (x0-x1) / (lambda/100);
-	const int xmesh_pec = (x1-x2) / (lambda/100);
+    const int xmesh_dielectric = (x0-x1) / (lambda/100);
+    const int xmesh_pec = (x1-x2) / (lambda/100);
     const int xmesh_slit = 2;
     const int xmesh_line = 4;
-	ofd_xsection(8, -x0, -x1, -x2, -x3, x3, x2, x1, x0);
-	ofd_xdivision(7, xmesh_dielectric, xmesh_pec, xmesh_slit, xmesh_line, xmesh_slit, xmesh_pec, xmesh_dielectric);
+    ofd_xsection(8, -x0, -x1, -x2, -x3, x3, x2, x1, x0);
+    ofd_xdivision(7, xmesh_dielectric, xmesh_pec, xmesh_slit, xmesh_line, xmesh_slit, xmesh_pec, xmesh_dielectric);
     
     // y
-	const double y0 = B/2;
-	const double y1 = L/2;
+    const double y0 = B/2;
+    const double y1 = L/2;
     const double y2 = y1 - l_de;
-	const int ymesh_dielectric =  (y0 - y1) / (lambda/100);
-	const int ymesh_slit = (l_de) / (lambda/100);
+    const int ymesh_dielectric =  (y0 - y1) / (lambda/100);
+    const int ymesh_slit = (l_de) / (lambda/100);
     const int ymesh_pec = (y1 + y2) / (lambda/100);
-	ofd_ysection(5, -y0, -y1, -y2, y1, y0);
-	ofd_ydivision(4,ymesh_dielectric, ymesh_slit, ymesh_pec, ymesh_dielectric);
+    ofd_ysection(5, -y0, -y1, -y2, y1, y0);
+    ofd_ydivision(4,ymesh_dielectric, ymesh_slit, ymesh_pec, ymesh_dielectric);
     // z
-	const double z0 = 0;
-	const double z1 = z0 + dt;
-	const double z2 = z1 + margin1;
-	const int zmesh_dt = 3;
-	const int zmesh_margin = (margin1) / (lambda/100);
-	ofd_zsection(4, -2e-3, z0, z1, z2);
-	ofd_zdivision(3, 2, zmesh_dt, zmesh_margin);
+    const double z0 = 0;
+    const double z1 = z0 + dt;
+    const double z2 = z1 + margin1;
+    const int zmesh_dt = 3;
+    const int zmesh_margin = (margin1) / (lambda/100);
+    ofd_zsection(4, -2e-3, z0, z1, z2);
+    ofd_zdivision(3, 2, zmesh_dt, zmesh_margin);
 
 
-	// material
+    // material
     ofd_material(er, sigma, 1, 0, "");
 
-	// geometry
+    // geometry
 
-	ofd_geometry(2, 1, -x0, x0, -y0, y0, z0, z1);       // substrate
+    ofd_geometry(2, 1, -x0, x0, -y0, y0, z0, z1);       // substrate
 
     ofd_geometry(1, 1, -x1, x1, -y1, y1, z1, z1);       
     ofd_geometry(2, 1, -x2, x2, -y1, -y2, z1, z1);
@@ -86,59 +86,59 @@ int main(void){
 
 
 
-	// feed
+    // feed
 
-	ofd_feed('Z', 0, -60e-3, 1.0e-3, 1, 0, 50);
+    ofd_feed('Z', 0, -60e-3, 1.0e-3, 1, 0, 50);
 
-	// point
-	ofd_point('Z', 0, -20e-3, 1.0e-3, "+Y");
+    // point
+    ofd_point('Z', 0, -20e-3, 1.0e-3, "+Y");
 
-	// ABC
+    // ABC
 
-	// ofd_pml(5, 2, 1.0e-5);
+    // ofd_pml(5, 2, 1.0e-5);
 
-	// frequency
+    // frequency
 
-	ofd_frequency1(2e-9, 3e-9, 200);
-	ofd_frequency2(freq, freq, 0);
+    ofd_frequency1(2e-9, 3e-9, 200);
+    ofd_frequency2(freq, freq, 0);
 
-	// solver
+    // solver
 
-	ofd_solver(30000, 1000, 1e-3);
+    ofd_solver(30000, 1000, 1e-3);
 
-	// iteration
+    // iteration
 
-	ofd_plotiter(1);
+    ofd_plotiter(1);
 
-	// spara
-	ofd_plotspara(1, 0, 0, 0);
+    // spara
+    ofd_plotspara(1, 0, 0, 0);
 
-	// far-1d
+    // far-1d
 
-	ofd_plotfar1d('Y', 360, 0);
+    ofd_plotfar1d('Y', 360, 0);
 
-	// output options
+    // output options
 
-	ofd_far1dstyle(0);
-	ofd_far1dcomponent(1, 0, 0);
-	ofd_far1ddb(1);
+    ofd_far1dstyle(0);
+    ofd_far1dcomponent(1, 0, 0);
+    ofd_far1ddb(1);
 
-	// near-2d
-	
-	ofd_plotnear2d("E",'Z',0);
-	ofd_near2ddim(1, 0);
-	ofd_near2dframe(10);
-	ofd_near2dscale(0, 1);
-	ofd_near2dcontour(0);
-	ofd_near2dobj(1);
-	ofd_near2dnoinc();
+    // near-2d
+    
+    ofd_plotnear2d("E",'Z',0);
+    ofd_near2ddim(1, 0);
+    ofd_near2dframe(10);
+    ofd_near2dscale(0, 1);
+    ofd_near2dcontour(0);
+    ofd_near2dobj(1);
+    ofd_near2dnoinc();
 
-	// window option
-	ofd_window2d(750, 500, 15, 0);
+    // window option
+    ofd_window2d(750, 500, 15, 0);
 
-	// output
+    // output
 
-	ofd_outdata("patch.ofd");
+    ofd_outdata("patch.ofd");
 
-	return 0;
+    return 0;
 }
